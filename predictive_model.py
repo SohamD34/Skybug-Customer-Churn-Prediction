478507cda8990d4bce31bf64377b7e8dc8a8ad75
Soham Deshmukh <deshmukh.2@iitj.ac.in>
2024-01-10 17:19:48 +0530

    Final commits

--- predictive_model.py
+++ predictive_model.py
@@ -18,15 +18,34 @@ def scale_inputs(features, max_vals, min_vals, data):
 # TAKING INPUTS
 
 print("Enter the following details to predict the churn probability of the customer:")
+
 country = input("Country (France, Spain, Germany): ")
+while(country not in ["France", "Spain", "Germany"]):
+    print("ERROR: Kindly enter valid country name.")
+    country = input("Country (France, Spain, Germany): ")
+
 credit_score = float(input("Credit Score: "))
+
 gender = input("Gender (Male, Female) :")
-age = float(input("Age: "))
+while(gender  not in ["Male", "Female"]):
+    print("ERROR: Kindly enter valid gender.")
+    gender = input("Gender (Male, Female) :")
+
+age = int(input("Age: "))
 tenure = int(input("Tenure: "))
 balance = float(input("Balance: "))
 num_of_products = int(input("Number of Products: "))
+
 has_credit_card = int(input("Has Credit Card (Yes:1, No:0): "))
+while(has_credit_card  not in [1,0]):
+    print("ERROR: Kindly enter valid credit card status.")
+    has_credit_card = int(input("Has Credit Card (Yes:1, No:0): "))
+
 is_active_member = int(input("Is Active Member (Yes:1, No:0): "))
+while(is_active_member  not in [1,0]):
+    print("ERROR: Kindly enter valid membership status.")
+    is_active_member = int(input("Is Active Member (Yes:1, No:0): "))
+
 estimated_salary = float(input("Estimated Salary: "))
 print("\n")
 
@@ -36,15 +55,11 @@ print("\n")
 
 input_data = [credit_score, country, gender, age, tenure, balance, num_of_products, has_credit_card, is_active_member, estimated_salary]
 
-print((input_data), "\n")
-
 input_df= pd.DataFrame([input_data], columns = ['CreditScore', 'Geography', 'Gender','Age','Tenure','Balance','NumOfProducts','HasCrCard','IsActiveMember', 'EstimatedSalary'])
 
 scaled_df = scale_inputs(features, max_vals, min_vals, input_df)
 scaled_df = country_mapping(scaled_df)
 scaled_df = gender_mapping(scaled_df)
-
-print(scaled_df, "\n")
 
 #______________________________________________________________________________
 
@@ -62,10 +77,10 @@ svm_lin_pred = svm_lin.predict(scaled_df)
 svm_sig_pred = svm_sig.predict(scaled_df)
 xgb_pred = xgb.predict(scaled_df)
 
-print("Predictions: (1 = Churn, 0 = No Churn)")
-print("Decision Tree Classifier: ", dtc_pred)
-print("Logistic Regression: ", lr_pred)
-print("Naive Bayes: ", nb_pred)
-print("Support Vector Machine (Linear Kernel): ", svm_lin_pred)
-print("Support Vector Machine (Sigmoid Kernel): ", svm_sig_pred)
-print("XGBoost: ", xgb_pred)
+print("PREDICTIONS: (1 = Churn, 0 = No Churn)\n")
+print("Decision Tree Classifier: ", dtc_pred[0])
+print("Logistic Regression: ", lr_pred[0])
+print("Naive Bayes: ", nb_pred[0])
+print("Support Vector Machine (Linear Kernel): ", svm_lin_pred[0])
+print("Support Vector Machine (Sigmoid Kernel): ", svm_sig_pred[0])
+print("XGBoost: ", xgb_pred[0])
